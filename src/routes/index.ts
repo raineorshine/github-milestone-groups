738cf1d79acb3e65f051765c5d3eded845971ab9
Raine Revere <raine@cybersemics.org>
2024-07-20 22:24:39 +0000

    Render groups in the correct positions.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,13 @@
 /** Sets up turbo route handlers and checks current route. */
 const route = (routes: { [key: string]: (...params: string[]) => void }) => {
+  let lastPathname: string
+
   const onRoute = () => {
+    // route handler can be triggered multiple times
+    // do not trigger route if url has not changed
+    if (window.location.pathname === lastPathname) return
+    lastPathname = window.location.pathname
+
     // @ts-ignore
     const [_, org, repo, page, ...params] = window.location.pathname.split('/')
     routes[page]?.(...params)
@@ -9,6 +16,8 @@ const route = (routes: { [key: string]: (...params: string[]) => void }) => {
   document.addEventListener('turbo:load', onRoute)
   const unsubscribe = () => document.removeEventListener('turbo:load', onRoute)
 
+  // Trigger the route handler in case turbo:load has already triggered.
+  // It will do nothing if the url has not changed.
   onRoute()
 
   return unsubscribe
--- src/routes/milestone.tsx
+++ src/routes/milestone.tsx
@@ -9,7 +9,8 @@ import storage from '../storage'
 
 /** A group of issues within a GitHub milestone. */
 interface MilestoneGroup {
-  startIssue: string
+  due: string
+  startIssue: number | null
 }
 
 /** A storage model type for all milestone groups keyed by GitHub milestone id. */
@@ -27,29 +28,45 @@ const db = storage.model({
 
 const store = ministore(db.get('milestones'))
 
+/** Decode an issue number from an issue row id. */
+const decodeIssueNumber = (el: Element | null) => (el?.id ? +el.id.split('_')[1] : null)
+
+/** Encode an issue number into an issue row id. */
+const encodeIssueNumber = (issueNumber: number) => `issue_${issueNumber}`
+
 /*****************************
  * COMPONENTS
  *****************************/
 
-const AddGroupLink = ({ milestoneId }: { milestoneId: string }) => {
+/** A link rendered in the milestone issues table that adds a new milestone group when clicked. */
+const NewGroupLink = ({ milestoneId }: { milestoneId: string }) => {
+  /** Creates a new group. */
+  const newGroup = () => {
+    // insert the milestone
+    const firstIssue = decodeIssueNumber(document.querySelector('.js-issue-row[id]'))
+    store.update(milestones => {
+      const groups = milestones[milestoneId] || []
+      return {
+        ...milestones,
+        // If this is the first group, set the startIssue to the first issue in the table.
+        // Otherwise, insert the new group in the penultimate position with startIssue: 0. Milestones are numbered in array order, so this is the same as adding the new group to the end, setting its startIssue to the issue below it, and setting the last group's startIssue to 0 since now has no issues.
+        [milestoneId]:
+          groups.length > 0
+            ? [...groups.slice(0, -1), { startIssue: null, due: '' }, groups.at(-1)!]
+            : [{ startIssue: firstIssue, due: '' }],
+      }
+    })
+  }
+
   return (
-    <a
-      onClick={() => {
-        store.update(milestones => ({
-          ...milestones,
-          [milestoneId]: [...(milestones[milestoneId] || []), { startIssue: 'x' }],
-        }))
-        renderMilestoneGroups(milestoneId)
-      }}
-      className='btn-link'
-      style={{ marginLeft: '1em', padding: 0 }}
-    >
-      + Add group
+    <a onClick={newGroup} className='btn-link' style={{ marginLeft: '1em', padding: 0 }}>
+      + New group
     </a>
   )
 }
 
-function Heading({ milestoneId, index }: { milestoneId: string; index: number }) {
+/** A milestone group heading. */
+function GroupHeading({ milestoneId, group, index }: { milestoneId: string; group: MilestoneGroup; index: number }) {
   const [showOptions, setShowOptions] = useState(false)
 
   return (
@@ -91,17 +108,20 @@ function Heading({ milestoneId, index }: { milestoneId: string; index: number })
           </div>
           {showOptions && (
             <div>
-              <div>Due: x</div>
+              <div>Due: {group.due}</div>
               <div>#xxx</div>
-              <div>$</div>
+              <div>$0</div>
               <div>
                 <a
                   onClick={() => {
+                    // delete group
                     store.update(state => {
-                      const milestones = state[milestoneId] || []
+                      const groups = state[milestoneId] || []
                       return {
                         ...state,
-                        [milestoneId]: milestones.filter((_, i) => i !== index),
+                        [milestoneId]: groups.filter(
+                          (_, i) => i !== (groups[index - 1].startIssue ? index : index - 1),
+                        ),
                       }
                     })
                   }}
@@ -124,58 +144,92 @@ function Heading({ milestoneId, index }: { milestoneId: string; index: number })
  *****************************/
 
 /** Renders milestone groups in the milestone issues table. */
-const renderMilestoneGroups = async (milestoneId: string) => {
+const renderGroups = (milestoneId: string) => {
   const container = document.querySelector('.js-milestone-issues-container')
   if (!container) {
     throw new Error('Unable to find .js-milestone-issues-container')
   }
 
-  // clear old milestone groups
+  // clear old milestone group elements
   container.querySelectorAll('.milestone-group').forEach(el => el.remove())
 
-  // get the current milestones
-  const milestones = store.getState()[milestoneId] || []
+  // get the current milestone groups
+  const groups = store.getState()[milestoneId] || []
 
-  // wait for the first issue to load before inserting the milestone groups
-  await waitForElement('.js-issue-row:not(.milestone-group)')
+  groups.forEach((group, i) => {
+    const nextSibling: HTMLElement | null = document.getElementById(
+      encodeIssueNumber(group.startIssue || groups.slice(i + 1).find(g => g.startIssue)?.startIssue || 0),
+    )
 
-  milestones.map((_, i) =>
-    insertReactRoot(container, 'insertBefore', {
+    return insertReactRoot(container, 'insertBefore', {
       className:
         'milestone-group Box-row Box-row--focus-gray js-navigation-item js-issue-row js-draggable-issue sortable-button-item',
-      nextSibling: container?.querySelector('.js-issue-row:not(.milestone-group)'),
+      nextSibling,
     })?.render(
       <React.StrictMode>
-        <Heading milestoneId={milestoneId} index={i} />
+        <GroupHeading milestoneId={milestoneId} group={group} index={i} />
       </React.StrictMode>,
-    ),
-  )
+    )
+  })
 }
 
 /** Inserts a React root into the table heading and renders milestone group options. */
-const renderMilestoneGroupOptions = (milestoneId: string) => {
+const renderOptionLinks = (milestoneId: string) => {
   insertReactRoot(
     document.getElementById('js-issues-toolbar')!.querySelector('.table-list-filters .table-list-header-toggle'),
     'appendChild',
     { tagName: 'span' },
   )?.render(
     <React.StrictMode>
-      <AddGroupLink milestoneId={milestoneId} />
+      <NewGroupLink milestoneId={milestoneId} />
     </React.StrictMode>,
   )
 }
 
+/** Updates the milestone groups based on thn currently rendered issues. Triggered after a milestrone is created or an issue is dragged to a new position. The DOM is the sourxe of truth, since GitHub controls interaction and rendering of the issues table. */
+const updateGroupsFromDOM = (milestoneId: string) => {
+  const groupRows = [...document.querySelectorAll('.milestone-group')]
+  const nextSiblings = groupRows.map(el => el.nextElementSibling)
+  const groups = nextSiblings.map(el => ({
+    startIssue: decodeIssueNumber(el),
+    due: '',
+  }))
+
+  // Only update state if it has deep changed, otherwise this can lead to an infinite render loop.
+  // Ministore only does a shallow compare.
+  if (JSON.stringify(groups) !== JSON.stringify(store.getState()[milestoneId])) {
+    store.update({
+      [milestoneId]: groups,
+    })
+  }
+}
+
 /** Renders milestone group options and milestone groups on the milestone page. */
-const milestone = (milestoneId: string) => {
-  // re-render everything
+const milestone = async (milestoneId: string) => {
+  // wait for the first issue to load before inserting the milestone groups
+  await waitForElement('.js-issue-row:not(.milestone-group)')
+
+  // persist and re-render milestone groups when the store changes
   store.subscribe(() => {
-    db.set('milestones', store.getState())
-    renderMilestoneGroups(milestoneId)
+    const state = store.getState()
+    db.set('milestones', state)
+    renderGroups(milestoneId)
+    setTimeout(() => {
+      updateGroupsFromDOM(milestoneId)
+    })
   })
 
   clear()
-  renderMilestoneGroupOptions(milestoneId)
-  renderMilestoneGroups(milestoneId)
+
+  renderOptionLinks(milestoneId)
+  renderGroups(milestoneId)
+
+  // update milestone on drag
+  // recalculate all milestones for simplicity
+  const container = document.querySelector('.js-milestone-issues-container')!
+  container.addEventListener('dragend', () => {
+    updateGroupsFromDOM(milestoneId)
+  })
 }
 
 export default milestone
